f71557d90ef598066188a616c3d86c8e7077994f
rule2003 <rllamas22@jaumebalmes.net>
2023-03-13 20:12:44 +0100

    trabajo de Raul Llamas Molano

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import json
 
 from IntegrantB.book import book
 from IntegrantB.cat import cat
+from IntegrantA.car import car
+from IntegrantA.school import school
 
 books = [
     book("El señor de los anillos", "J.r.r. Tolkien", "dura", "488", "Novela Fantastica", "MINOTAURO"),
@@ -26,4 +28,32 @@ cats_list= [c.to_dict() for c in cats]
 data = {"books":books_list, "cats":cats_list}
 
 with open("jsonAPI/data.json", 'w') as file:
+    json.dump(data, file)
+
+
+
+
+cars = [
+    car("Seat", "Ateca", "2022", "España"),
+    car("Audi", "Q7", "2017", "Alemania"),
+    car("BMW", "320d", "2018", "Alemania"),
+    car("Ford", "Fiesta", "2023", "Estados Unidos"),
+    car("Toyota", "RAV4", "2015", "Japon")
+]
+
+schools = [
+    school("Jaume Balmes", "Barcelona", "España", "777", "Eso", "5"),
+    school("La guineueta", "Barcelona", "España", "1778", "Bachillerato", "4"),
+    school("Quatre Cantons", "Barcelona", "España", "582", "Eso", "3"),
+    school("Xarau", "Cerdanyola del Valles", "España", "33", "Primaria", "2"),
+    school("La Llacuna", "Barcelona", "España", "333", "Primaria", "4")
+]
+
+cars_list = [c.to_dict() for c in cars]
+
+schools_list = [s.to_dict() for s in schools]
+
+data = {"cars":cars_list, "schools":schools_list}
+
+with open("jsonAPI/data1.json", "w") as file:
     json.dump(data, file)
